a9b1e193418a9aaeade0af161c62b4cdc936b9e1
kbannach <krzysztof92b@gmail.com>
2017-03-17 22:30:00 +0100

    small fixes

--- src/main/resources/run_benchmark.sh
+++ src/main/resources/run_benchmark.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# TODO
+# 1. java args from script execution erguments
+# 2. loop
+
 # create temporary files
 cp -r ../java/* .
 # build
@@ -7,15 +12,18 @@ javac main.Main
 # create results file
 touch results.txt
 printf "heap size: 128 MB\n" >> results.txt
-printf "GC algorithm\t1 thd, fixed size\t1 thd, random size\tn thds, fixed obj size\tn thds, random obj size\t" >> results.txt
+printf "GC algorithm\t1 thd, fixed size\t1 thd, random size\tn thds, fixed size\tn thds, random size\t" >> results.txt
 
 # run benchmark
 printf "ParrallelOld\t"
-java -Xms128m -Xmx128m -XX:+UseParallelOldGC main.Main -allocations=10000000 -arrSize=1
-printf "CMS\t"
-java -Xms128m -Xmx128m -XX:+UseParallelOldGC main.Main -allocations=10000000 -arrSize=1
-printf "G1\t"
-java -Xms128m -Xmx128m -XX:+UseParallelOldGC main.Main -allocations=10000000 -arrSize=1
+java -Xms128m -Xmx128m -XX:+UseParallelOldGC main.Main -allocations=10000000 -arrSize=300
+java -Xms128m -Xmx128m -XX:+UseParallelOldGC main.Main -allocations=10000000 -arrSize=-1
+java -Xms128m -Xmx128m -XX:+UseParallelOldGC main.Main -allocations=10000000 -arrSize=300 -threads=4
+java -Xms128m -Xmx128m -XX:+UseParallelOldGC main.Main -allocations=10000000 -arrSize=-1 -threads=4
+#printf "CMS\t"
+#java -Xms128m -Xmx128m -XX:+UseConcMarkSweepGC main.Main -allocations=10000000 -arrSize=1
+#printf "G1\t"
+#java -Xms128m -Xmx128m -XX:+UseG1GC main.Main -allocations=10000000 -arrSize=1
 
 
 # garbage collection
