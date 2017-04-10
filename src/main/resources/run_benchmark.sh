59e243b6d55fd992331006563a9f06df817be18b
kbannach <krzysztof92b@gmail.com>
2017-04-10 18:54:56 +0200

    added average execution time measurement

--- src/main/resources/run_benchmark.sh
+++ src/main/resources/run_benchmark.sh
@@ -19,9 +19,15 @@ view_usage() {
 
 exec_java() {
     # $1 algorithm; $2 heap size; $3 allocations; $4 arrSize; $5 threads
-
+    
+    totalTime=0
     printf "executing: java "$2" -XX:+Use"$1"GC main.Main "$3" "$4" "$5" \n"
-    sh -c "java "$2" -XX:+Use"$1"GC main.Main "$3" "$4" "$5" \n" >> results.txt
+    for i in `seq 1 10`;
+    do
+	t=$(sh -c "java "$2" -XX:+Use"$1"GC main.Main "$3" "$4" "$5"")
+	totalTime=$(( totalTime + t ))
+    done
+    printf "avg: "$(( totalTime / 10 )) >> results.txt
 }
 
 # begin script
@@ -59,7 +65,7 @@ algorithms=(ParallelOld ConcMarkSweep G1)
 heap_sizes=(128 256 512)
 for hs in ${heap_sizes[*]}; do
     printf "heap size: "$hs" MB\n" >> results.txt
-    printf "GC algorithm\t1 thd, fixed size\t1 thd, random size\tn thds, fixed size\tn thds, random size\n" >> results.txt
+    printf "GC algorithm\t\t1 thd, fixed size\t1 thd, random size\tn thds, fixed size\tn thds, random size\n" >> results.txt
 
     heap_size='-Xms'$hs'm -Xmx'$hs'm'
     for alg in ${algorithms[*]}; do
@@ -72,11 +78,11 @@ for hs in ${heap_sizes[*]}; do
 	# run commands
 	# $1 algorithm; $2 heap size; $3 allocations; $4 arrSize; $5 threads
 	exec_java $alg $heap_size $allocations $arrSize '' ;
-	printf "\t\t\t" >> results.txt ;
+	printf "   \t\t" >> results.txt ;
 	exec_java $alg $heap_size $allocations '-arrSize=-1' '' ;
-	printf "\t\t\t" >> results.txt ;
+	printf "   \t\t" >> results.txt ;
 	exec_java $alg $heap_size $allocations $arrSize $threads ;
-	printf "\t\t\t" >> results.txt ;
+	printf "   \t\t" >> results.txt ;
 	exec_java $alg $heap_size $allocations '-arrSize=-1' $threads ;
 	printf "\n" >> results.txt ;
     done
